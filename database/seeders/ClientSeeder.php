e0b7838da0d4666c1109ec861d976f0966f22922
Denice Jane Aparicio <denicejaneaparicio1402@gmail.com>
2023-07-13 22:50:57 +0800

    email_aparicio

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -13,6 +13,33 @@ class ClientSeeder extends Seeder
      */
     public function run(): void
     {
-        Client::factory(30)->create();
+        Client::factory(20)->create();
+        $cl = [
+            [
+                'last_name' => 'Aparicio',
+                'first_name' => 'Denice Jane',
+                'middle_name' => 'Bautista',
+                'address' => 'Centro2, Baogo, Inabanga, Bohol',
+                'birth_date' => '2002-10-14',
+                'phone_number' => '09203522715',
+                'email' => 'denicejaneaparicio1402@gmail.com',
+                'initial_deposit' => 10000
+            ],
+            [
+                'last_name' => 'Caminus',
+                'first_name' => 'Apple',
+                'middle_name' => 'Jane',
+                'address' => ' Korea',
+                'birth_date' => '2001-6-03',
+                'phone_number' => '090238423784',
+                'email' => 'denicejaneaparicio1402@gmail.com',
+                'initial_deposit' => 10000
+            ]
+
+        ];
+
+        foreach($cl as $c) {
+            Client::create($c);
+        }
     }
 }
